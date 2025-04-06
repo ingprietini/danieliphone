093d911b21789d396f5541dfa5de4b285516884b
ingprietini <angeljafetvaldiviamaldonado@gmail.com>
2025-04-05 23:48:28 -0600

    final

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -7,9 +7,7 @@ const AboutUs = () => {
         <div className="flex flex-col md:flex-row items-center justify-center gap-12">
           <div className="w-full md:w-1/3">
             <div className="flex justify-center">
-              <svg className="h-48 w-48 text-lyra-purple" viewBox="0 0 24 24" fill="currentColor">
-                <path d="M12,2C6.5,2,2,6.5,2,12c0,3.8,2.1,7.2,5.3,8.8c0.8-1.1,2.1-1.8,3.6-1.8c0.7,0,1.4,0.2,2,0.5c0.6-0.3,1.2-0.5,1.9-0.5c1.4,0,2.8,0.7,3.6,1.8c3.2-1.6,5.3-4.9,5.3-8.8C22,6.5,17.5,2,12,2z M12,15.5c-1.9,0-3.5-1.6-3.5-3.5s1.6-3.5,3.5-3.5s3.5,1.6,3.5,3.5S13.9,15.5,12,15.5z"/>
-              </svg>
+            <img src="/imagenes/logo.png" style="with:50%;" alt="logotipo" />
             </div>
           </div>
           <div className="w-full md:w-2/3">
